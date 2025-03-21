adccbe0b2851660cc5b0de843ead0eba240f6843
Gabriel Henriques <fc58182@alunos.fc.ul.pt>
2025-03-21 19:27:24 +0000

    added week3 files

---
+++ week3/myFirstParser/Java/LangPrintAST.java
@@ -0,0 +1,32 @@
+/**
+ * Técnicas de Compilação
+ * Mestrado em Engenharia Informática
+ * Faculdade de Ciências
+ * Universidade de Lisboa
+ * 2024/2025
+ * 
+ * As per "The Definitive ANTLR 4 Reference",
+ * but using CharStreams in place of the deprecated ANTLRInputStream
+ * 
+ * See Lang.g4 on how to generate Java from a g4 grammar. Then
+ * 
+ * Run as:
+ * $ java LangPrintAST < example.lang 
+ */
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class LangPrintAST {
+    public static void main(String[] args) throws Exception {
+        // create a CharStream that reads from standard input
+        CharStream input = CharStreams.fromStream(System.in);
+        // create a lexer that feeds off of input CharStream
+        LangLexer lexer = new LangLexer(input);
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // create a parser that feeds off the tokens buffer
+        LangParser parser = new LangParser(tokens);
+        ParseTree tree = parser.lang(); // begin parsing at init rule
+        System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+    }
+}
