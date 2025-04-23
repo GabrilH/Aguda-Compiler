e43154d9ad0b859e72abd71d13ad039151bf2fa2
Gabriel Henriques <fc58182@alunos.fc.ul.pt>
2025-04-23 12:41:41 +0100

    changed minor stuff

--- aguda-compiler/src/type_checker.py
+++ aguda-compiler/src/type_checker.py
@@ -30,33 +30,33 @@ class ErrorLogger:
     def get_errors(self):
         return self.messages
                 
-def checkEqualTypes(exp: Exp, actual_type: Type, expected_type: Type):
+def checkEqualTypes(error_exp: Exp, actual_type: Type, expected_type: Type):
     """
     Checks if two types are equal or compatible.
     """
     if actual_type != expected_type:
-        logger.log(f"Expected two equal types; found {expected_type} and {actual_type}, "
-                   f"for expression \n'{exp}'", exp.lineno, exp.column)
+        logger.log(f"Expected two equal types; found {actual_type} and {expected_type}, "
+                   f"for expression \n'{error_exp}'", error_exp.lineno, error_exp.column)
     
-def checkInstance(exp: Exp, actual_type: Type, expected_class: type) -> bool:
+def checkInstance(error_exp: Exp, actual_type: Type, expected_class: type) -> bool:
     """
     Checks if the actual type is an instance of the expected class.
     Returns True if it is, otherwise logs an error and returns False.
     """
     if not isinstance(actual_type, expected_class):
         logger.log(f"Expected a {expected_class.__name__}, found {actual_type} "
-                   f"for expression \n'{exp}'", exp.lineno, exp.column)
+                   f"for expression \n'{error_exp}'", error_exp.lineno, error_exp.column)
         return False
     return True
 
-def checkBuiltInConflict(exp: Exp, var: Var):
+def checkBuiltInConflict(error_exp: Exp, var: Var):
     """
     Checks if the variable name conflicts with built-in functions.
     """
     if var.name in ['print', 'length']:
-        logger.log(f"Variable name '{var}' conflicts with built-in function", exp.lineno, exp.column)
+        logger.log(f"Variable name '{var}' conflicts with built-in function", error_exp.lineno, error_exp.column)
 
-def checkArguments(ctx: SymbolTable, matched_exp: Exp, exps: List[Exp], function_type: FunctionType):
+def checkArguments(ctx: SymbolTable, error_exp: Exp, exps: List[Exp], function_type: FunctionType):
     """
     Checks if the arguments `exps` match the expected types in `expected_types`.
     """
@@ -65,31 +65,31 @@ def checkArguments(ctx: SymbolTable, matched_exp: Exp, exps: List[Exp], function
     if len(actual_types) != len(expected_types) or any(a != e for a, e in zip(actual_types, expected_types)):
         logger.log(
             f"expected arguments of types [{', '.join(str(t) for t in expected_types)}], "
-            f"found [{', '.join(str(t) for t in actual_types)}] for expression \n'{matched_exp}'",
-            matched_exp.lineno,
-            matched_exp.column
+            f"found [{', '.join(str(t) for t in actual_types)}] for expression \n'{error_exp}'",
+            error_exp.lineno,
+            error_exp.column
         )
 
-def checkParemeters(function_id : Var, parameters: List[Var], function_type: FunctionType):
+def checkParemeters(error_exp : Exp, parameters: List[Var], function_type: FunctionType):
     """
     Checks if the parameters names are unique and do not conflict with built-in functions.
     """
     if len(parameters) != len(function_type.param_types):
         logger.log(f"number of params does not match type in function declaration "
-                   f"'{function_id}'", function_id.lineno, function_id.column)
+                   f"'{error_exp}'", error_exp.lineno, error_exp.column)
 
     param_names = set()
     for param in parameters:
         if param.name != '_' and param.name in param_names:
             logger.log(f"duplicate parameter name '{param.name}' in function declaration "
-                       f"'{function_id}'", function_id.lineno, function_id.column)
+                       f"'{error_exp}'", error_exp.lineno, error_exp.column)
         param_names.add(param.name)
 
-def checkAgainst(ctx: SymbolTable, match_exp: Exp, expected_type: Type) -> None:
+def checkAgainst(ctx: SymbolTable, matched_exp: Exp, expected_type: Type) -> None:
     """
     Checks if the expression `exp` matches the expected type `type` in the given context `ctx`.
     """
-    match (match_exp, expected_type):
+    match (matched_exp, expected_type):
         case (IntLiteral(), BaseType('Int')):
             pass
         case (BoolLiteral(), BaseType('Bool')):
@@ -107,42 +107,42 @@ def checkAgainst(ctx: SymbolTable, match_exp: Exp, expected_type: Type) -> None:
             checkAgainst(ctx, exp2, BaseType('Int'))
             exp1Type : ArrayType = typeof(ctx, exp1)
             if checkInstance(exp1, exp1Type, ArrayType):
-                checkEqualTypes(match_exp, exp1Type.type, expected_type)
+                checkEqualTypes(matched_exp, exp1Type.type, expected_type)
 
         case (FunctionCall(id, exps), _):
             match (id.name, expected_type):
                 case ('print', BaseType('Unit')):
                     if len(exps) != 1:
-                        logger.log(f"Print function takes exactly one argument", match_exp.lineno, match_exp.column)
+                        logger.log(f"Print function takes exactly one argument", matched_exp.lineno, matched_exp.column)
                     else:
                         exp1 = exps[0]
                         typeof(ctx, exp1)
 
                 case ('length', BaseType('Int')):
                     if len(exps) != 1:
-                        logger.log(f"Length function takes exactly one argument", match_exp.lineno, match_exp.column)
+                        logger.log(f"Length function takes exactly one argument", matched_exp.lineno, matched_exp.column)
                     else:
                         exp1 = exps[0]
                         exp1Type = typeof(ctx, exp1)
-                        checkInstance(match_exp, exp1Type, ArrayType)
+                        checkInstance(matched_exp, exp1Type, ArrayType)
 
                 case ('length', _) | ('print', _):
-                    logger.log(f"Expected type '{expected_type}', found type '{typeof(ctx, match_exp)}' "
-                               f"for expression \n'{match_exp}'", match_exp.lineno, match_exp.column)
+                    logger.log(f"Expected type '{expected_type}', found type '{typeof(ctx, matched_exp)}' "
+                               f"for expression \n'{matched_exp}'", matched_exp.lineno, matched_exp.column)
 
                 case _:
                     funcType : FunctionType = typeof(ctx, id)
                     if checkInstance(id, funcType, FunctionType):
-                        checkArguments(ctx, match_exp, exps, funcType)
-                        checkEqualTypes(match_exp, funcType.return_type, expected_type)
+                        checkArguments(ctx, matched_exp, exps, funcType)
+                        checkEqualTypes(matched_exp, funcType.return_type, expected_type)
 
         case (VariableDeclaration(id, type, exp),BaseType('Unit')) | (TopLevelVariableDeclaration(id, type, exp),BaseType('Unit')):
             checkAgainst(ctx, exp, type)
             insertIntoCtx(ctx, id, type)
 
         case (Var(name),_):
-            actual_type = typeofVar(ctx, match_exp, name)
-            checkEqualTypes(match_exp, actual_type, expected_type)
+            actual_type = typeofVar(ctx, matched_exp, name)
+            checkEqualTypes(matched_exp, actual_type, expected_type)
         
         case (Conditional(exp1, exp2, exp3), _):
             checkAgainst(ctx, exp1, BaseType('Bool'))
@@ -166,18 +166,18 @@ def checkAgainst(ctx: SymbolTable, match_exp: Exp, expected_type: Type) -> None:
             if operator in ['+', '-', '*', '/', '%', '^']:
                 checkAgainst(ctx, left, BaseType('Int'))
                 checkAgainst(ctx, right, BaseType('Int'))
-                checkEqualTypes(match_exp, BaseType('Int'), expected_type)
+                checkEqualTypes(matched_exp, BaseType('Int'), expected_type)
             
             if operator in ['==', '!=', '<', '>', '<=', '>=']:
                 leftType = typeof(ctx, left)
                 rightType = typeof(ctx, right)
-                checkEqualTypes(match_exp, leftType, rightType)
-                checkEqualTypes(match_exp, BaseType('Bool'), expected_type)
+                checkEqualTypes(matched_exp, leftType, rightType)
+                checkEqualTypes(matched_exp, BaseType('Bool'), expected_type)
             
             if operator in ['&&', '||']:
                 checkAgainst(ctx, left, BaseType('Bool'))
                 checkAgainst(ctx, right, BaseType('Bool'))
-                checkEqualTypes(match_exp, BaseType('Bool'), expected_type)
+                checkEqualTypes(matched_exp, BaseType('Bool'), expected_type)
 
         case (LogicalNegation(operand), BaseType('Bool')):
             checkAgainst(ctx, operand, BaseType('Bool'))
@@ -186,10 +186,10 @@ def checkAgainst(ctx: SymbolTable, match_exp: Exp, expected_type: Type) -> None:
             checkAgainst(ctx, exp, expected_type)
 
         case _:
-            logger.log(f"Expected type '{expected_type}', found type '{typeof(ctx, match_exp)}' "
-                       f"for expression \n'{match_exp}'", match_exp.lineno, match_exp.column)
-            # actual_type = typeof(ctx, match_exp)
-            # checkEqualTypes(match_exp, actual_type, expected_type)
+            logger.log(f"Expected type '{expected_type}', found type '{typeof(ctx, matched_exp)}' "
+                       f"for expression \n'{matched_exp}'", matched_exp.lineno, matched_exp.column)
+            # actual_type = typeof(ctx, matched_exp)
+            # checkEqualTypes(matched_exp, actual_type, expected_type)
 
 def insertIntoCtx(ctx: SymbolTable, var: Var, type: Type) -> None:
     """
@@ -205,11 +205,11 @@ def typeofVar(ctx: SymbolTable, exp: Exp, name: str) -> Type:
         logger.log(f"unresolved symbol: {name}", exp.lineno, exp.column)
     return varType
     
-def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
+def typeof(ctx: SymbolTable, matched_exp: Exp) -> Type:
     """
-    Determines the type of the expression `match_exp` in the given context `ctx`.
+    Determines the type of the expression `matched_exp` in the given context `ctx`.
     """
-    match match_exp:        
+    match matched_exp:        
         case IntLiteral():
             return BaseType('Int')
         
@@ -237,22 +237,22 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
         case FunctionCall(id, exps):
             if id.name == 'print':
                 if len(exps) != 1:
-                    logger.log(f"Print function takes exactly one argument", match_exp.lineno, match_exp.column)
+                    logger.log(f"Print function takes exactly one argument", matched_exp.lineno, matched_exp.column)
                 exp1 = exps[0]
                 typeof(ctx, exp1)
                 return BaseType('Unit')
             
             if id.name == 'length':
                 if len(exps) != 1:
-                    logger.log(f"Length function takes exactly one argument", match_exp.lineno, match_exp.column)
+                    logger.log(f"Length function takes exactly one argument", matched_exp.lineno, matched_exp.column)
                 exp1 = exps[0]
                 exp1Type = typeof(ctx, exp1)
-                checkInstance(match_exp, exp1Type, ArrayType)
+                checkInstance(matched_exp, exp1Type, ArrayType)
                 return BaseType('Int')
             
             funcType : FunctionType = typeof(ctx, id)
             if checkInstance(id, funcType, FunctionType):
-                checkArguments(ctx, match_exp, exps, funcType)
+                checkArguments(ctx, matched_exp, exps, funcType)
                 return funcType.return_type
             #TODO return a quê caso nao seja function?
             
@@ -265,13 +265,13 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
             return BaseType('Unit')
         
         case Var(name):
-            return typeofVar(ctx, match_exp, name)
+            return typeofVar(ctx, matched_exp, name)
         
         case Conditional(exp1, exp2, exp3):
             checkAgainst(ctx, exp1, BaseType('Bool'))
             exp2Type = typeof(ctx.enter_scope(), exp2)
             exp3Type = typeof(ctx.enter_scope(), exp3)         
-            checkEqualTypes(match_exp, exp2Type, exp3Type)          
+            checkEqualTypes(matched_exp, exp2Type, exp3Type)          
             return exp2Type
         
         case WhileLoop(exp1, exp2):
@@ -282,7 +282,7 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
         case Assignment(lhs, exp):
             lhsType = typeof(ctx, lhs)
             expType = typeof(ctx, exp)
-            checkEqualTypes(match_exp, lhsType, expType)
+            checkEqualTypes(matched_exp, lhsType, expType)
             return BaseType('Unit')
         
         case Sequence(first, rest):
@@ -299,7 +299,7 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
             if operator in ['==', '!=', '<', '>', '<=', '>=']:
                 leftType = typeof(ctx, left)
                 rightType = typeof(ctx, right)
-                checkEqualTypes(match_exp, leftType, rightType)
+                checkEqualTypes(matched_exp, leftType, rightType)
                 return BaseType('Bool')
             
             if operator in ['&&', '||']:
@@ -317,8 +317,8 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
             return BaseType('Bool')
         
         case FunctionDeclaration(id, parameters, type, body):
-            checkBuiltInConflict(match_exp, id)
-            if not checkInstance(match_exp, type, FunctionType):
+            checkBuiltInConflict(matched_exp, id)
+            if not checkInstance(matched_exp, type, FunctionType):
                 return
             type : FunctionType = type
             checkParemeters(id, parameters, type)
@@ -336,7 +336,7 @@ def typeof(ctx: SymbolTable, match_exp: Exp) -> Type:
             return typeof(ctx.enter_scope(), exp)
         
         case _:
-            logger.log(f"Unknown expression type '{type(match_exp)}'", match_exp.lineno, match_exp.column)
+            logger.log(f"Unknown expression type '{type(matched_exp)}'", matched_exp.lineno, matched_exp.column)
         
 def second_pass(ctx: SymbolTable, program: Program) -> None:
     """
